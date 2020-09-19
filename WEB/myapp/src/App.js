01db1caa74c37f0a8d636ac40d2b836cc93aa25e
lijiaonago <823571683@qq.com>
2020-09-19 22:23:52 +0800

    "后台管理系统界面设计"

--- WEB/myapp/src/App.js
+++ WEB/myapp/src/App.js
@@ -1,29 +1,298 @@
-import React from 'react';
+import React,{Suspense, lazy }from 'react';
 import logo from './logo.svg';
-import './App.css';
-import { withRouter } from 'react-router-dom';
+import './App.scss';
+import { HashRouter, BrowserRouter, Route, Redirect, Switch, Link, NavLink, withRouter } from 'react-router-dom';
+import { Layout, Menu, Breadcrumb } from 'antd';
+import { 
+  HomeOutlined,
+  FileDoneOutlined,
+  ScheduleOutlined,
+  FieldTimeOutlined,
+  FileOutlined,
+  ContactsOutlined,
+  FileTextOutlined,
+  ProfileOutlined,
+  ReadOutlined,
+  TagsOutlined,
+  SolutionOutlined,
+  UserAddOutlined,
+  TableOutlined,
+  FileAddOutlined,
+  DatabaseOutlined,
+  UserOutlined,
+  InsertRowAboveOutlined,
+} from '@ant-design/icons';
+const { SubMenu } = Menu;
+const { Header, Content, Sider } = Layout;
+// @withRouter
 
-@withRouter
-class App extends React.Component {
+//引入页面组件
+const Home = lazy(() => import("./views/Home"));
+const ToDone = lazy(() => import("./views/ToDone"));
+const ToDoListAll = lazy(() => import("./views/ToDoListAll"));
+const ToDoneAll = lazy(() => import("./views/ToDoneAll"));
+const ToDoneAllYes = lazy(() => import("./views/ToDoneAllYes"));
+const VacDays = lazy(() => import("./views/VacDays"));
+const PersonnelMan = lazy(() => import("./views/PersonnelMan"));
+const VacMan = lazy(() => import("./views/VacMan"));
+const AdminMan = lazy(() => import("./views/AdminMan"));
+const Other = lazy(() => import("./views/Other"));
+const UserList = lazy(() => import("./views/UserList"));
+const UserAdd = lazy(() => import("./views/UserAdd"));
+const DepartmentList = lazy(() => import("./views/DepartmentList"));
+const DepartmentAdd = lazy(() => import("./views/DepartmentAdd"));
+
+
+// import Home from './views/Home'
+// import ToDone from './views/ToDone'
+// import ToDoListAll from './views/ToDoListAll'
+// import ToDoneAll from './views/ToDoneAll'
+// import ToDoneAllYes from './views/ToDoneAllYes'
+// import VacDays from './views/VacDays'
+// import PersonnelMan from './views/PersonnelMan'
+// import VacMan from './views/VacMan'
+// import AdminMan from './views/AdminMan'
+// import Other from './views/Other'
+// import UserList from './views/UserList'
+// import UserAdd from './views/UserAdd'
+// import DepartmentList from './views/DepartmentList'
+// import DepartmentAdd from './views/DepartmentAdd'
+
+class App extends React.PureComponent {
   state = {
-    num: 10
+    /* 一级菜单 */
+    menu:[{
+        text:"我的首页",
+        name:"Home",
+        icon:<HomeOutlined />,
+        path:'/Home',
+        
+      },{
+        text:"我的已办",
+        name:"ToDone",
+        icon:<FileDoneOutlined />,
+        path:'/ToDone'
+      },
+    ],
+    /* 二级菜单 */
+    secmenu:[
+      [
+        {
+          text:"我的待办",
+          name:"ToDoList",
+          icon:<ScheduleOutlined />,
+        },
+        {
+          text:"待办事项总数",
+          name:"ToDoListAll",
+          icon:<FieldTimeOutlined />,
+          path:'/ToDoListAll',
+        },
+        {
+          text:"已办事项总数",
+          name:"ToDoneAll",
+          icon:<FileOutlined />,
+          path:'/ToDoneAll',
+        },
+        {
+          text:"昨日已办总数",
+          name:"ToDoneAllYes",
+          icon:<FileOutlined />,
+          path:'/ToDoneAllYes',
+        },
+        {
+          text:"本月休假天数",
+          name:"VacDays",
+          icon:<ContactsOutlined />,
+          path:'/VacDays',
+        }
+      ],
+      [
+        {
+          text:"发起事件",
+          name:"InitiatingMatters",
+          icon:<FileAddOutlined />,
+        },
+        {
+          text:"人事管理",
+          name:"PersonnelMan",
+          icon:<FileTextOutlined />,
+          path:'/PersonnelMan'
+        },
+        {
+          text:"假勤管理",
+          name:"VacMan",
+          icon:<ProfileOutlined />,
+          path:'/VacMan'
+        },
+        {
+          text:"行政管理",
+          name:"AdminMan",
+          icon:<ReadOutlined />,
+          path:'/AdminMan'
+        },
+        {
+          text:"其他",
+          name:"Other",
+          icon:<TagsOutlined />,
+          path:'/Other'
+        },
+      ],
+      [
+        {
+          text:"用户管理",
+          name:"UserManagement",
+          icon:<UserOutlined />,
+        },
+        {
+          text:"用户列表",
+          name:"UserList",
+          icon:<SolutionOutlined />,
+          path:'/UserList',
+        },
+        {
+          text:"用户添加",
+          name:"UserAdd",
+          icon:<UserAddOutlined />,
+          path:'/UserAdd',
+        },
+      ],
+      [
+        {
+          text:"部门管理",
+          name:"DepartmentAdmin",
+          icon:<DatabaseOutlined />,
+        },
+        {
+          text:"部门列表",
+          name:"DepartmentList",
+          icon:<TableOutlined />,
+          path:'/DepartmentList'
+        },
+        {
+          text:"部门添加",
+          name:"DepartmentAdd",
+          icon:<InsertRowAboveOutlined />,
+          path:'/DepartmentAdd'
+        },
+      ],
+    ],
+    collapsed: false,
   }
-  changeNumber = () => {
+
+
+  gotoPage = ({ key }) => {
     this.setState({
-      num: this.state.num + 1
+        current:key
     })
+    this.goto(key);
+    // this.props.history.replace(path);
   }
+  goto = (path)=>{
+      this.props.history.push(path);
+  }
+  
+  onCollapse = collapsed => {
+    console.log(collapsed);
+    this.setState({ collapsed });
+  }
+
   render() {
     console.log('App.props',this.props)
+    const { menu, secmenu } = this.state;
     return (
       <div className="App">
-        Hello Test {this.state.num}
+         <Layout>
+          <Header className="header">
+            <div className="logo">
+              <img src="../logo.png" alt=""/>
+              <span>后台管理系统</span>
+            </div>
+          </Header>
+          <Layout style={{ minHeight: '100vh' }}>
+              <Sider collapsible collapsed={this.state.collapsed} onCollapse={this.onCollapse}>
+              <div className="logo" />
+                <Menu onClick={this.gotoPage}
+                theme="dark" defaultSelectedKeys={['1']} >
+                  {
+                    menu.map(item => 
+                    <Menu.Item key={item.path} icon={item.icon}>
+                        {item.text}
+                    </Menu.Item>
+                    )
+                  }
+                  {/* 渲染二层菜单 */}
+                  {secmenu.map((item, idx) => {
+                    return (
+                      <SubMenu
+                        key={item.path}
+                        icon={item[0].icon}
+                        title={item[0].text}
+                      >
+                        {item.map((secitem, index) => {
+                          // console.log("secitem", secitem, index);
+                          if (index != 0) {
+                            return (
+                              <Menu.Item key={secitem.name} icon={secitem.icon}>
+                                {secitem.text}
+                              </Menu.Item>
+                            );
+                          }
+                        })}
+                      </SubMenu>
+                    );
+                  })}
+                </Menu>
+            </Sider>
+            <Layout style={{ padding: '0 24px 24px'}}>
+              {/* <Breadcrumb style={{ margin: '16px 0' }}>
+                <Breadcrumb.Item>Home</Breadcrumb.Item>
+                <Breadcrumb.Item>List</Breadcrumb.Item>
+                <Breadcrumb.Item>App</Breadcrumb.Item>
+              </Breadcrumb> */}
+              <Content
+                className="site-layout-background"
+                style={{
+                  padding: 10,
+                  margin: 0,
+                  minHeight: 280,
+                }}
+              >
+                <Suspense fallback={<div>loading...</div>}>
+                  <Switch>
+                  <Route path="/Home" component={Home} />
+                  <Route path="/ToDone" component={ToDone} />
+                  <Route path="/ToDoListAll" component={ToDoListAll} />
+                  <Route path="/ToDoneAll" component={ToDoneAll} />
+                  <Route path="/ToDoneAllYes" component={ToDoneAllYes} />
+                  <Route path="/VacDays" component={VacDays} />
+                  <Route path="/PersonnelMan" component={PersonnelMan} />
+                  <Route path="/VacMan" component={VacMan} />
+                  <Route path="/AdminMan" component={AdminMan} />
+                  <Route path="/Other" component={Other} />
+                  <Route path="/UserList" component={UserList} />
+                  <Route path="/UserAdd" component={UserAdd} />
+                  <Route path="/DepartmentList" component={DepartmentList} />
+                  <Route path="/DepartmentAdd" component={DepartmentAdd} />
+                  <Route path="/notfound" render={() => <div>404</div>} />
 
-        <button onClick={this.changeNumber}>change Number</button>
+                  {/* <Route path="/iq/:id" component={IQ} /> */}
+                  <Redirect from="/" to="/Home" exact />
+                  {/* 404 */}
+                  {/* <Redirect to="/notfound" /> */}
+                </Switch>
+              </Suspense>
+              
+              </Content>
+            </Layout>
+          </Layout>
+        </Layout>
+        
       </div>
     );
   }
 
 }
 
+App = withRouter(App);
 export default App;
