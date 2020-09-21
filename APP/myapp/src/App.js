fca1642dec7e9524f8f21715d3c418dca0eb178c
李明珍 <1746934940@qq.com>
2020-09-21 16:10:13 +0800

    antd-mobil按需引入

--- APP/myapp/src/App.js
+++ APP/myapp/src/App.js
@@ -2,8 +2,15 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import { withRouter } from 'react-router-dom';
+import { Grid } from 'antd-mobile';
+// import 'antd-mobile/dist/antd-mobile.css';
 
-@withRouter
+const data = Array.from(new Array(9)).map((_val, i) => ({
+  icon: 'https://gw.alipayobjects.com/zos/rmsportal/nywPmnTAvTmLusPxHPSu.png',
+  text: `name${i}`,
+}));
+
+ @withRouter
 class App extends React.Component {
   state = {
     num: 10
@@ -16,10 +23,15 @@ class App extends React.Component {
   render() {
     console.log('App.props',this.props)
     return (
-      <div className="App">
-        Hello Test {this.state.num}
+      <div>
+      {/* <div className="App">
+          Hello Test {this.state.num}
 
-        <button onClick={this.changeNumber}>change Number</button>
+          <button onClick={this.changeNumber}>change Number</button>
+        </div> */
+      }
+        <div className="sub-title">这里是logo</div>
+        <Grid data={data} activeStyle={false} />
       </div>
     );
   }
